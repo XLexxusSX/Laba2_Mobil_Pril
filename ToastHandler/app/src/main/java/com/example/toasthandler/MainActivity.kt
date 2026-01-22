2f94e0243ba583fcdfb9c035a27413a9f5885d8a
Alexey.Popov <lexxuss2008@gmail.com>
2026-01-23 02:54:12 +0700

    Лаба 2. Задача 1 выполнена

--- ToastHandler/app/src/main/java/com/example/toasthandler/MainActivity.kt
+++ ToastHandler/app/src/main/java/com/example/toasthandler/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_ok)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_LONG).show()  // LENGTH_LONG ≈ 2 секунды
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
